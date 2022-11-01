6b6b413060c27fbcc44e09f655925296967333df
mqpanda <daniilpraktika@gmail.com>
2022-11-01 17:54:17 +0300

    add: header and search-block

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="wrapper clear">
+      <header className='d-flex justify-between align-center'>
+        <div className='header-left'>
+          <div className='logo'>
+            <span className='fly'>fly</span>time
+          </div>
+        </div>
+        <ul className='d-flex header-right'>
+          <li>Flights</li>
+          <li>Packages</li>
+          <li>Settings</li>
+          <li>Sign in</li>
+          <li><button className='sign cu-p'>Sign up</button></li>
+        </ul>
       </header>
+
+
+
+      <div className='flight-search'>
+        <input placeholder='From where?' >
+          
+        </input>
+        <input placeholder='Where-to' >
+        </input>
+        <input type="date">
+        
+        </input>
+        <input placeholder='1 adult'>
+        
+        </input>
+        <button>
+          Search
+        </button>
+        
+        
+      </div>  
+
     </div>
   );
 }
