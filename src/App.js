a3ef0718301b5332a57fced38e2c25bba1986c15
mqpanda <daniilpraktika@gmail.com>
2022-11-04 14:33:29 +0300

    add: intro and search-box

--- src/App.js
+++ src/App.js
@@ -18,27 +18,42 @@ function App() {
         </ul>
       </header>
 
+      <div className='intro'>
+        <div>
+          <img src='/IMG/intro.png' className='wind' alt='intro'></img>
+        </div>
+        <div className='question'>
+          <h1>Ready for an <span className='fly'>adventure?</span></h1>
+        </div>
+
+        <div className='flight-search'>
+          <div className='from'>
+            <img className='icon' width={21} height={18} src='/IMG/Union.svg' alt='union'/>
+            <h6>From where?</h6>
+          </div>
+          <div className='from'>
+            <img className='icon' width={21} height={18} src='/IMG/Union.svg' alt='union'/>
+            <h6>Where to?</h6>
+          </div>
+          <div className='from'>
+            <img className='icon' width={21} height={18} src='/IMG/Union.svg' alt='union'/>
+            <h6>Depart - Return</h6>
+          </div>
+          <div className='from'>
+            <img className='icon' width={21} height={18} src='/IMG/Union.svg' alt='union'/>
+            <h6>1 adult</h6>
+          </div>
+          <div className='from'>
+            <div className='btn'>
+              Search
+            </div>
+          </div>
+        </div>
+      </div>
 
 
-      <div className='flight-search'>
-        <input placeholder='From where?' >
-          
-        </input>
-        <input placeholder='Where-to' >
-        </input>
-        <input type="date">
-        
-        </input>
-        <input placeholder='1 adult'>
-        
-        </input>
-        <button>
-          Search
-        </button>
-        
-        
-      </div>  
 
+      
     </div>
   );
 }
